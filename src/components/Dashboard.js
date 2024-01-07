ca8ffc472fb9c3a67f369878c4e3bb56d321af2a
Muskaan <muskaanshakilshaikh721@gmail.com>
2024-01-07 14:13:42 +0530

    Started With Login And Register Page

---
+++ src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Welcome to the IPO and Currency Dashboard!</h1>
+      <p>This is your dashboard where you can view upcoming IPOs and currency exchange rates.</p>
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Login = () => {
+    console.log('Login component rendered');
+  return (
+    <div>
+      <h2>Login Page</h2>
+      
+    </div>
+  );
+};
+
+export default Login;
